acdb96874a4501765f72dae948dad60a12b12807
David Hormigo Ramírez <dhorram948@g.educaand.es>
2023-10-31 13:00:58 +0100

    * Añadir botones de compartir y editar * Crear manejador evento * Definir intent de compartir * Ampliar adaptador

--- app/src/main/java/com/alanturing/cpifp/todo/MainActivity.kt
+++ app/src/main/java/com/alanturing/cpifp/todo/MainActivity.kt
@@ -4,10 +4,12 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.activity.result.contract.ActivityResultContracts
 import com.alanturing.cpifp.todo.adapter.TasksAdapter
 import com.alanturing.cpifp.todo.data.TaskLocalRepository
 import com.alanturing.cpifp.todo.databinding.ActivityMainBinding
+import com.alanturing.cpifp.todo.model.Task
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
@@ -16,7 +18,7 @@ class MainActivity : AppCompatActivity() {
     private val getResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
 
         when (it.resultCode) {
-            Activity.RESULT_OK -> binding.tasks.adapter = TasksAdapter(repository)
+            Activity.RESULT_OK -> binding.tasks.adapter = TasksAdapter(repository,::onShareTask)
             Activity.RESULT_CANCELED -> Snackbar.make(this,binding.createTaskButton,"Tarea no creada",Snackbar.LENGTH_SHORT)
                 .show()
         }
@@ -25,10 +27,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.tasks.adapter = TasksAdapter(repository)
+        binding.tasks.adapter = TasksAdapter(repository,::onShareTask)
         binding.createTaskButton.setOnClickListener {
             val intent = Intent(this,CreateTodoActivity::class.java)
             getResult.launch(intent)
         }
+    }
+    private fun onShareTask(task: Task, view: View) {
+        //Toast.makeText(view.context,"Pulsado",Toast.LENGTH_SHORT).show()
+        val intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            type = "text/plain"
+            putExtra(Intent.EXTRA_TEXT,getString(R.string.share_text,task.title,task.description))
+        }
+        val shareIntent = Intent.createChooser(intent,null)
+        startActivity(shareIntent)
+
     }
 }
--- app/src/main/java/com/alanturing/cpifp/todo/adapter/TasksAdapter.kt
+++ app/src/main/java/com/alanturing/cpifp/todo/adapter/TasksAdapter.kt
@@ -1,19 +1,24 @@
 package com.alanturing.cpifp.todo.adapter
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.alanturing.cpifp.todo.data.TaskLocalRepository
 import com.alanturing.cpifp.todo.databinding.TodoItemBinding
 import com.alanturing.cpifp.todo.model.Task
 
-class TasksAdapter(private val repository: TaskLocalRepository): RecyclerView.Adapter<TasksAdapter.TaskViewHolder>() {
+class TasksAdapter(private val repository: TaskLocalRepository,
+    private val onShareTask:(t:Task,v: View)->Unit): RecyclerView.Adapter<TasksAdapter.TaskViewHolder>() {
     inner class TaskViewHolder(private val binding: TodoItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bindTask(t: Task) {
             binding.title.text = t.title
             binding.description.text = t.description
             binding.isCompleted.isChecked = t.isCompleted
+            binding.btnShare.setOnClickListener {
+                onShareTask(t,it)
+            }
         }
     }
 
